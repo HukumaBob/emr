4ec5cc542ddcd840914228907de118f2e0f099c1
Nikita <ldc.endoscopy@gmail.com>
2023-05-24 13:04:49 +0400

    README

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  <div id="app">
-  {{ patients }}
-</div>
+  <div>
+    <h1>List of Patients</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Middle Name</th>
+          <th>Last Name</th>
+          <th>Date of Birth</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="patient in patients" :key="patient.id">
+          <td>{{ patient.first_name }}</td>
+          <td>{{ patient.middle_name }}</td>
+          <td>{{ patient.last_name }}</td>
+          <td>{{ patient.date_of_birth }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
 
 <script>
 import axios from 'axios';
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import axios from 'axios';
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
 
 // Set the base URL for axios requests
 axios.defaults.baseURL = 'http://localhost:8000/api/v1/';
